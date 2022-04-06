ccdf11320073a5da32aaa329944293a261f50b45
Robert Kirui <kirugik79@gmail.com>
2022-04-06 21:54:28 +0300

    Create input for new tasks with 'add task' button, and 'delete task' button for task list items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'To-Do-List';
+  title = 'To Do List';
+  taskList: any [] = [];
+
+  addNewTask(task: string) {
+    this.taskList.push({id:this.taskList.length, name:task});
+  }
+
+  deleteTask(id: number) {
+    this.taskList = this.taskList.filter(task => task.id !== id);//remove the task associated with id provided (the current task)
+  }
 }

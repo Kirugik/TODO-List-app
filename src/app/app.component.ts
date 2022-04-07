09a325eb18030c6693c56c1b299f95f9b95fe868
Robert Kirui <kirugik79@gmail.com>
2022-04-07 11:52:43 +0300

    Create textarea for task description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'To Do List';
+  title = 'My To-Do List';
   taskList: any [] = [];
 
   addNewTask(task: string) {
